7367e9743f24fddfe6df01a5dbb4af9d0e95efa7
Sumayea Akter Samia <140933858+22201075@users.noreply.github.com>
2023-08-12 18:39:30 +0600

    Update 1010.c

--- 1010.c
+++ 1010.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    char name;
+    double unit,price,totalAmount;
+
+    scanf("%s %lf %lf",&name,&unit,&price);
+    totalAmount = unit*price;
+
+    scanf("%s %lf %lf",&name,&unit,&price);
+    totalAmount += unit*price;
+
+    printf("VALOR A PAGAR: R$ %.2lf\n",totalAmount);
+    return 0;
+}
