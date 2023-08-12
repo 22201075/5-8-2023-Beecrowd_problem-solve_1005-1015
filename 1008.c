9d0e783af0f1914a5383708e8d8dcdab385c37b2
Sumayea Akter Samia <140933858+22201075@users.noreply.github.com>
2023-08-12 18:38:23 +0600

    Update 1008.c

--- 1008.c
+++ 1008.c
@@ -1,1 +1,11 @@
 
+#include <stdio.h>
+
+int main() {
+    int id;
+    double hours,perHourSalary,totalSalary;
+    scanf("%d %lf %lf",&id,&hours,&perHourSalary);
+    totalSalary = hours*perHourSalary;
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n",id,totalSalary);
+    return 0;
+}
