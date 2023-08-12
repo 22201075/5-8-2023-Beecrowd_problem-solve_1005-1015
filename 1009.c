94713e1e867f969b7c3a6a057c0cc1c796ca399f
Sumayea Akter Samia <140933858+22201075@users.noreply.github.com>
2023-08-12 18:38:53 +0600

    Update 1009.c

--- 1009.c
+++ 1009.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name;
+    double salary,sales,totalSalary;
+    scanf("%s %lf %lf",&name,&salary,&sales);
+    totalSalary = (15*sales)/100 + salary;
+    printf("TOTAL = R$ %.2lf\n",totalSalary);
+    return 0;
+}
 
