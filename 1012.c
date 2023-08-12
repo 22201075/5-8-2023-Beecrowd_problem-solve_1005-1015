450ee8c8338923e0b0a210e84d556455fe546dd2
Sumayea Akter Samia <140933858+22201075@users.noreply.github.com>
2023-08-12 18:40:24 +0600

    Update 1012.c

--- 1012.c
+++ 1012.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    double a,b,c,pi=3.14159,triangleArea,circleArea,trapeziumArea,squareArea,rectangleArea;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    triangleArea = (1.00/2)*a*c;
+    circleArea = pi*c*c;
+    trapeziumArea = (1.00/2)*(a+b)*c;
+    squareArea = b*b;
+    rectangleArea = a*b;
+    printf("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n",triangleArea,circleArea,trapeziumArea,squareArea,rectangleArea);
+    return 0;
+}
